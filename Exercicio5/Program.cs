48e35a8f96b6cd7cce25f4f502f1640147db5654
Gabriel Gulke <ggulke070@gmail.com>
2022-07-03 19:13:54 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercicio 5 \nNúmeros pares entre 1 e 100");
+
+            for (int i = 1; i <= 50; i++)
+                Console.WriteLine(i * 2);
+            Console.ReadKey();
+        }
+    }
+}
